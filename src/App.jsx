856d5ef9de94f9b67afe2196e319aa02c31bf04f
María Paz <llavedesol16@gmail.com>
2025-09-06 15:49:39 -0400

    Update (App.jsx): get users from async function

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,22 @@ function App() {
 
   const JsonPlaceholder = () => fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(users => {
+        users.forEach(user => console.log(user.name))
+      })
+      .catch(error => console.log(error))
+
+    async function obtenerUsuarios() {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const users = await response.json()
+        users.forEach(user => console.log(user.name))
+      } catch (error) {
+        console.log("Error: ",error)
+      }
+    }
+
+    obtenerUsuarios()
 
   return (
     <>
